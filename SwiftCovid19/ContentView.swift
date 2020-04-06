4b8ab9bfc92bb9c67a124f827487e1de3a76dfe1
indogusmas <indogusmasarungsamudra@gmail.com>
2020-04-06 21:32:36 +0700

    Initial Commit

---
+++ SwiftCovid19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftCovid19
+//
+//  Created by indo gusmas arung samudra on 06/04/20.
+//  Copyright © 2020 indo gusmas arung samudra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
